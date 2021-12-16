db65dbf57140cb9ae752092a898522266de75007
yiminghe <yiminghe@gmail.com>
2021-12-16 13:47:53 +0800

    update

--- run.js
+++ run.js
@@ -9,12 +9,14 @@ const v = (require('child_process').execSync('yarn -v', {
 const v1 = v.startsWith('1.');
 
 module.exports = function (env, registry) {
-  if (v1) {
-    env.yarn_registry = registry;
-  } else {
-    env.yarn_npm_Registry_Server = registry;
-    // env.yarn_node_linker = 'node-modules';
-    // env.yarn_pnp_mode = 'loose';
+  if (registry) {
+    if (v1) {
+      env.yarn_registry = registry;
+    } else {
+      env.yarn_npm_Registry_Server = registry;
+      // env.yarn_node_linker = 'node-modules';
+      // env.yarn_pnp_mode = 'loose';
+    }
   }
   const processEnv = process.env;
   for (const k in processEnv) {
