a8b904b62dad0a49a71b4c4c041b30dae041a3c7
yiminghe <yiminghe@gmail.com>
2021-12-30 16:09:42 +0800

    add env

--- env.js
+++ env.js
@@ -1,16 +1,3 @@
-const MIRROR_URL = 'https://npmmirror.com/mirrors';
+const config = require('binary-mirror-config');
 
-const env = {
-  NODEJS_ORG_MIRROR: `${MIRROR_URL}/node`,
-  NVM_NODEJS_ORG_MIRROR: `${MIRROR_URL}/node`,
-  NVM_IOJS_ORG_MIRROR: `${MIRROR_URL}/iojs`,
-  PHANTOMJS_CDNURL: `${MIRROR_URL}/phantomjs`,
-  CHROMEDRIVER_CDNURL: `${MIRROR_URL}/chromedriver/`.replace('https://', 'http://'),
-  ELECTRON_MIRROR: `${MIRROR_URL}/electron/`,
-  SASS_BINARY_SITE: `${MIRROR_URL}/node-sass`,
-  OPERADRIVER_CDNURL: `${MIRROR_URL}/operadriver`,
-  PUPPETEER_DOWNLOAD_HOST: MIRROR_URL,
-  npm_config_canvas_binary_host_mirror:`${MIRROR_URL}/node-canvas-prebuilt`,
-};
-
-module.exports = env;
+module.exports = config.china.ENVS;
