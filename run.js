795efcad0099bf2e672c61e99a468e59b9a2b14a
yiminghe <yiminghe@gmail.com>
2022-01-02 20:33:36 +0800

    use js-yaml

--- run.js
+++ run.js
@@ -1,32 +1,42 @@
 'use strict';
 
 const spawn = require('cross-spawn');
+const path = require('path');
 
 const v = (require('child_process').execSync('yarn -v', {
   encoding: 'utf-8'
 }));
 
+const cwd = process.cwd();
+
 const v1 = v.startsWith('1.');
 
+const yaml = require('js-yaml');
+const fs = require('fs');
+
 module.exports = function (env, registry) {
+  const processEnv = process.env;
+  let { yarn_npm_Registry_Server } = processEnv;
+  const ymlPath = path.join(cwd, '.yarnrc.yml');
+  if (!v1 && !yarn_npm_Registry_Server && fs.existsSync(ymlPath)) {
+    const doc = yaml.load(fs.readFileSync(ymlPath, 'utf8')) || {};
+    yarn_npm_Registry_Server = doc.npmRegistryServer;
+  }
   if (registry) {
     if (v1) {
       env.yarn_registry = registry;
     } else {
-      env.yarn_npm_Registry_Server = registry;
-      // env.yarn_node_linker = 'node-modules';
-      // env.yarn_pnp_mode = 'loose';
+      env.yarn_npm_Registry_Server = yarn_npm_Registry_Server || registry;
     }
   }
-  const processEnv = process.env;
-  for (const k in processEnv) {
-    if (!processEnv.hasOwnProperty(k) || env[k]) {
-      continue;
-    }
-    env[k] = processEnv[k];
+  if (!v1) {
+    console.log('npmRegistryServer:', env.yarn_npm_Registry_Server);
   }
   spawn('yarn', process.argv.slice(2), {
-    env: env,
+    env: {
+      ...processEnv,
+      ...env,
+    },
     cwd: process.cwd(),
     stdio: 'inherit',
   }).on('exit', function (code) {
